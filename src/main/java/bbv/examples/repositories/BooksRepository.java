8788882fa7fda3abb3c7ccc0e0e3fd94c5060aa3
Olavi <olavi.tiimus@bbv.eu>
2020-01-14 10:59:36 +0100

    changes for comments and formatting exercise

--- src/main/java/bbv/examples/repositories/BooksRepository.java
+++ src/main/java/bbv/examples/repositories/BooksRepository.java
@@ -1,9 +1,7 @@
 package bbv.examples.repositories;
-
 import bbv.examples.domain.Book;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -18,36 +16,30 @@ public class BooksRepository {
   public Collection<Book> findAll() {
     return books.values();
   }
-
   public Book findById(Integer id) {
     return books.get(id);
   }
-
   public Book save(Book book) {
-    return book.getId() != null ? this.update(book) : this.add(book);
+    if (book.getId() != null)
+      return this.update(book);
+    else
+    return this.add(book);
   }
-
   private Book add(Book book) {
       Date createdAt = new Date();
-
       book.setId(books.size() + 1);
       book.setCreatedAt(createdAt);
       book.setUpdatedAt(createdAt);
-
       books.put(book.getId(),
         book);
-
       return book;
   }
 
   private Book update(Book book) {
     book.setUpdatedAt(new Date());
-
     books.put(book.getId(), book);
-
     return book;
   }
-
   public void delete(Book book) {
     books.remove(book.getId());
   }
